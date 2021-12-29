4c789bbef453435567d4e4b2dbfc461187dc87c3
Zhenya <eugeneas@gmail.com>
2021-12-29 05:50:16 -0800

    fix: change esm support

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+set -euo pipefail
 tsc
-tsc -p tsconfig.esm.json
+mkdir -p dist/esm
 cat >dist/cjs/package.json <<!EOF
 {
     "type": "commonjs"
@@ -11,4 +12,10 @@ cat >dist/esm/package.json <<!EOF
 {
     "type": "module"
 }
+!EOF
+
+cat >dist/esm/index.js <<!EOF
+export * from "../cjs/index.js";
+import mod from "../cjs/index.js";
+export default mod['default'];
 !EOF
